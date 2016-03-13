bc86f9d56250c1efb963647da59a9c08e9a199bf
mharvey92 <ma.harvey1050@gmail.com>
2016-03-13 11:07:24 -0400

    fixed NA and Merge problem

--- km_code.R
+++ km_code.R
@@ -15,15 +15,18 @@ head(data)
 names<-data.frame(data$Id2, data$Geography)
 data<-data[,3:44]
 
+## Set seed
+set.seed(98)
+
 ## Perform Principal Component Analysis
 pcadata<- prcomp(data, center=TRUE, scale. = TRUE)
 percentvar<-pcadata$sd^2/sum(pcadata$sd^2)*100
 percentvar[1:3]
 screeplot(pcadata)
 plot(pcadata, type="lines")
-
+pcomp <- data.frame(pcadata$x[,1:3])
 ##Scale data
-data_scaled<- scale(data) 
+##data_scaled<- scale(data) 
 
 # Determine number of clusters for k means
 wss <- (nrow(data)-1)*sum(apply(data,2,var))
@@ -33,11 +36,11 @@ plot(1:10, wss, type="b", xlab="# of Clusters",
      ylab="Within groups sum of squares")
 
 # K-Means Cluster Analysis with k=3
-fit <- kmeans(data_scaled, 3) # 5 cluster solution
+fit <- kmeans(pcomp, 3) 
 # get cluster means 
-aggregate(data_scaled,by=list(fit$cluster),FUN=mean)
+aggregate(pcomp,by=list(fit$cluster),FUN=mean)
 ##Plot clusters against 2 comps
-mydata<-data.frame(data_scaled, fit$cluster)
+mydata<-data.frame(pcomp, fit$cluster)
 clusplot(mydata, fit$cluster, color=TRUE, shade=TRUE, 
          labels=2, lines=0)
 
@@ -62,7 +65,8 @@ for(x in affected){
 ## read in employment variable = Y and make delta Y
 employment<-read.csv("employment0809.csv")
 head(employment)
-knamesdata$deltaemploy<-employment$delta_employ
+head(knamesdata)
+knamesdata <- merge(knamesdata,employment,by="data.Id2")
 
 ## Add in race variables
 controls<-read.csv("Data_Collection.csv")
@@ -78,6 +82,8 @@ knamesdata$gender_ratio<-controls$Total..Estimate..Total.population...SUMMARY.IN
 knamesdata$education<-controls$Total..Estimate..Population.25.years.and.over...Bachelor.s.degree
 knamesdata$married<-controls$Now.married..except.separated...Estimate..Population.15.years.and.over
 head(knamesdata)
+## Only complete cases (theres should be 5 less obs because of 2008 employment var)
+knamesdata<-knamesdata[complete.cases(knamesdata),]
 
 ## Seperate out data by k-clusters
 group1<-filter(knamesdata, fit.cluster==1)
@@ -86,9 +92,9 @@ group3<-filter(knamesdata, fit.cluster==3)
 
 ## Descriptive statistics for clusters
 library(stargazer)
-group1table<-stargazer(group1[,6:13])
-group2table<-stargazer(group2[,6:13])
-group3table<-stargazer(group3[,6:13])
+group1table<-stargazer(group1[,9:16])
+group2table<-stargazer(group2[,9:16])
+group3table<-stargazer(group3[,9:16])
 
 ## Create models
 ##Group 1
@@ -98,13 +104,18 @@ group1_3<-lm(deltaemploy~statedummy+White+Black+Hispanic+Asian+education+married
 ##Group 2
 group2_1<-lm(deltaemploy~statedummy, data=group2)
 group2_2<-lm(deltaemploy~statedummy+White+Black+Hispanic+Asian, data=group2)
-group2_3<-lm(deltaemploy~statedummy+White+Black+Hispanic+Asian+median_age+gender_ratio+education+married, data=group2)
+group2_3<-lm(deltaemploy~statedummy+White+Black+Hispanic+Asian+education+married, data=group2)
 ## Group 3
 group3_1<-lm(deltaemploy~statedummy, data=group3)
 group3_2<-lm(deltaemploy~statedummy+White+Black+Hispanic+Asian, data=group3)
-group3_3<-lm(deltaemploy~statedummy+White+Black+Hispanic+Asian+median_age+gender_ratio+education+married, data=group3)
+group3_3<-lm(deltaemploy~statedummy+White+Black+Hispanic+Asian+education+married, data=group3)
+##All Metro Groups
+group4_1<-lm(deltaemploy~statedummy, data=knamesdata)
+group4_2<-lm(deltaemploy~statedummy+White+Black+Hispanic+Asian, data=knamesdata)
+group4_3<-lm(deltaemploy~statedummy+White+Black+Hispanic+Asian+education+married, data=knamesdata)
 
 ##Create output tables
-stargazer(group1_1, group1_2, group1_3, title="Difference-In-Difference: Group 1")
-stargazer(group2_1, group2_2, group2_3, title="Difference-In-Difference: Group 2")
-stargazer(group3_1, group3_2, group3_3, title="Difference-In-Difference: Group 3")
+stargazer(group1_1, group1_2, group1_3, title="Difference-In-Difference for Group 1")
+stargazer(group2_1, group2_2, group2_3, title="Difference-In-Difference for Group 2")
+stargazer(group3_1, group3_2, group3_3, title="Difference-In-Difference for Group 3")
+stargazer(group4_1, group4_2, group4_3, title="Difference-In-Difference for All Metro Areas")
